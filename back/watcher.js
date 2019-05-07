d59efac0c7ae77d830849b2324d17919876716ce
Arto Haponen <haponen@gmail.com>
2019-05-07 23:40:18 +0200

    [TASK & FEAT] Simplify code & add date based file storing

---
+++ back/watcher.js
@@ -0,0 +1,81 @@
+const https = require("https");
+const fs = require("fs-extra");
+
+const QUOTES = [
+  {
+    currency: "BTC",
+    path: "/markets/bittrex/dogebtc/price"
+  },
+  {
+    currency: "USDT",
+    path: "/markets/huobi/dogeusdt/price"
+  }
+];
+
+const TEMPLATE = () =>
+  QUOTES.map(quote => ({
+    currency: quote.currency,
+    priceHistory: [],
+    price: 0
+  }));
+
+const PRICE_INFO_DIR = "back/price_history/";
+const currentStoreFileName = new Date().toISOString().split("T")[0] + ".json";
+const currentStore = (() => {
+  try {
+    return JSON.parse(
+      fs.readFileSync(`${PRICE_INFO_DIR}${currentStoreFileName}`)
+    );
+  } catch (error) {
+    console.log("File not found, returning template");
+    return TEMPLATE();
+  }
+})();
+
+console.log("Much watch. So info.");
+setInterval(() => {
+  QUOTES.map(quote => {
+    https
+      .request(
+        {
+          path: quote.path,
+          method: "GET",
+          protocol: "https:",
+          hostname: "api.cryptowat.ch"
+        },
+        res => {
+          let body = "";
+          res.on("data", chunk => {
+            body += chunk;
+          });
+          res.on("end", () => {
+            console.log("Wow", body);
+
+            const price = JSON.parse(body).result.price;
+            const exactCurrency = currentStore.find(
+              currency => currency.currency === quote.currency
+            );
+
+            exactCurrency.price = price;
+            exactCurrency.priceHistory.push({
+              price,
+              time: Date.now()
+            });
+
+            fs.writeFile(
+              PRICE_INFO_DIR + currentStoreFileName,
+              JSON.stringify(currentStore),
+              err => {
+                if (err) console.log(err);
+              }
+            );
+          });
+        }
+      )
+      .on("error", err => {
+        console.log(err);
+        fs.appendFile("./logs/error.log", `${err}\n`);
+      })
+      .end();
+  });
+}, 1000 * 60);
--- build.js
+++ build.js
@@ -2,9 +2,6 @@ const fs = require("fs-extra");
 
 (async function build() {
   try {
-    await fs.emptyDir("./dist");
-    await fs.copy("./src/front/", "./dist");
-    await fs.copy("./src/back/price_history/", "./dist/price_history");
     console.log('So build!')
   } catch (error) {
     console.log(error);
