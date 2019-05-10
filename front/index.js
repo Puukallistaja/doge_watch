28504df5f7e7259054ae9646b7c7cc855d88e992
Arto Haponen <haponen@gmail.com>
2019-05-10 15:04:31 +0200

    [TASK] Move scripts and styles to separat files

---
+++ front/index.js
@@ -0,0 +1,35 @@
+let priceInfo = [
+  {
+    currency: "DOGE",
+    price: 1
+  }
+];
+const currentStoreFileName =
+  new Date().toISOString().split("T")[0] + ".json";
+fetch(`price_history/${currentStoreFileName}`)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(priceJson) {
+    if (priceJson.length) {
+      priceInfo = priceInfo.concat(priceJson);
+      localStorage.setItem("currencies", JSON.stringify(priceJson));
+    }
+  })
+  .catch(err => {
+    console.log(err, "Will try to use old priceinfo");
+    priceInfo = localStorage.getItem("currencies");
+  })
+  .finally(addListeners);
+
+function addListeners() {
+  document.querySelector("select").addEventListener("change", event => {
+    try {
+      document.querySelector(".price").innerText = priceInfo
+        .find(currency => currency.currency === event.target.value)
+        .price.toFixed(8);
+    } catch (error) {
+      console.log(error, "\n Failed to change price");
+    }
+  });
+}
